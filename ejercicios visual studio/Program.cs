92779eea591400c0624f111daf0b5708724dc67c
rene765 <reneboza7@gmail.com>
2020-02-19 12:07:15 +0100

    Create Program.cs
    preoyecto 1

---
+++ ejercicios visual studio/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Introduzca un número: ");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Introduzca un segundo número: ");
+            int y = int.Parse(Console.ReadLine());
+            if (x > y)
+            {
+
+                Console.Write("es mayor ");
+                
+            }
+            else
+            {
+                Console.Write("es menor ");
+
+            }
+            
+
+
+        }
+    }
+}
